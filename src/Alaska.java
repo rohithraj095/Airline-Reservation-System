eefdd89cf25257a294fc3bfc12e026805fc4a4ad
cdeckowitz <54671916+cdeckowitz@users.noreply.github.com>
2019-11-20 20:19:15 -0500

    Update Alaska.java

--- src/Alaska.java
+++ src/Alaska.java
@@ -4,11 +4,38 @@ public class Alaska implements Airline {
 
     private ArrayList<Passenger> passengers;
 
-    public Alaska(Passenger passenger) {
-
+    public Alaska() {
         passengers = new ArrayList<>();
+    }
 
-        if(passenger != null)
+    public void addPassengers(Passenger passenger) {
+        if(passenger != null && getCurrentPassengers() < getMaxPassengers())
             passengers.add(passenger);
     }
+
+    @Override
+    public int getCurrentPassengers() {
+        return passengers.size();
+    }
+
+    @Override
+    public int getMaxPassengers() {
+        return 100;
+    }
+
+    @Override
+    public String toString() {
+        return "Alaska Airlines is proud to serve the strong and knowledgeable Boilermakers from Purdue University. \n" +
+                "We primarily fly westward, and often have stops in Alaska and California. \n" +
+                "We have first class amenities, even in coach class. \n" +
+                "We provide fun snacks, such as pretzels and goldfish. \n" +
+                "We also have comfortable seats and free WiFi. \n" +
+                "We hope you choose Alaska Airlines for your next trip!";
+    }
+
+    public String getGate() {
+        Gate gate = new Gate();
+        return gate.toString();
+    }
 }
+
