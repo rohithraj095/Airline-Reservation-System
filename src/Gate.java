0d91d1b6e96301ad11ca6e68792d2eed8fecd743
rohithraj095 <rohithraj095@gmail.com>
2019-11-22 14:52:36 -0600

    Update Gate.java

--- src/Gate.java
+++ src/Gate.java
@@ -1,4 +1,34 @@
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Random;
 
 public class Gate implements Serializable {
+
+    private int randNum;
+    private ArrayList<String> alphas;
+    private String alpha;
+
+    public Gate() {
+
+        Random rand = new Random();
+
+        alphas = new ArrayList<String>();
+
+        alphas.add("A");
+        alphas.add("B");
+        alphas.add("C");
+
+        int randAlpha = rand.nextInt(alphas.size());
+
+        alpha = alphas.get(randAlpha);
+
+        alphas.remove(randAlpha);
+
+        randNum = rand.nextInt(18);
+        randNum++;
+    }
+
+    public String toString(){
+        return alpha + randNum;
+    }
 }
