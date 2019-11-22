a8dff99863115ef0e69bc67956d8d89a734243d0
rohithraj095 <rohithraj095@gmail.com>
2019-11-22 14:51:42 -0600

    Update Passenger.java

--- src/Passenger.java
+++ src/Passenger.java
@@ -3,11 +3,13 @@ public class Passenger {
     private String fName;
     private String lName;
     private int age;
+    private Airline airline;
 
-    public Passenger(String fName, String lName, int age) {
+    public Passenger(String fName, String lName, int age, Airline airline) {
         this.fName = fName;
         this.lName = lName;
         this.age = age;
+        this.airline = airline;
     }
 
     public String getfName() {
@@ -20,5 +22,13 @@ public class Passenger {
 
     public int getAge() {
         return age;
+    }
+
+    public Airline getAirline(){
+        return airline;
+    }
+
+    public String toString(){
+        return fName.substring(0,1).toUpperCase() + ". " + lName.toUpperCase() + ", " + age;
     }
 }
