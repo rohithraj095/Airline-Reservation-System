1a6c5e9728f11382591b55a5069d64873248691b
rohithraj095 <rohithraj095@gmail.com>
2019-11-22 14:50:41 -0600

    Update BoardingPass.java

--- src/BoardingPass.java
+++ src/BoardingPass.java
@@ -1,2 +1,16 @@
 public class BoardingPass {
+
+    private Passenger passenger;
+
+    public BoardingPass(Passenger passenger){
+        this.passenger = passenger;
+    }
+
+    public String toString(){
+        return "BOARDING PASS FOR FLIGHT 18000 WITH " + passenger.getAirline() + "\n" +
+                "PASSENGER FIRST NAME " + passenger.getfName().toUpperCase() + "\n" +
+                "PASSENGER LAST NAME " + passenger.getlName().toUpperCase() + "\n" +
+                "PASSENGER AGE " + passenger.getAge();// + "\n" +
+                //"You can now begin boarding at gate " + passenger.getAirline().getGatetoString();
+    }
 }
